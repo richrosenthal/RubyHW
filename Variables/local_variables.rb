2217ed19e311117e620516b86d6d426fc56cf6a0
richrosenthal <rrosenthal2013@gmail.com>
2021-04-19 21:34:10 -0400

    create local variable example

---
+++ Variables/local_variables.rb
@@ -0,0 +1,9 @@
+# local variables
+
+first_name = "Ricky"
+last_name = "Rosenthal"
+message = "Wubba lubba dub dub"
+_space = " "
+full_message = first_name + _space + last_name + _space + message
+
+puts(full_message)
